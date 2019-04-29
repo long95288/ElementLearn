c95a5a098aa035324da211c0e960a967e81ec8bd
longquanxiao <1186481819@qq.com>
2019-04-30 00:12:12 +0800

    router
    配置简单路由

--- src/main.js
+++ src/main.js
@@ -1,7 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import VueRouter from 'vue-router'
+
+Vue.use(VueRouter)
+
+import Layout from './components/Layout'
+
+
+
+const router = new VueRouter({
+  routes :[
+    {name:'Layout',path:'/Layout',component:Layout}
+  ]
+})
+
+// 配置路由
+
+
 new Vue({
   el: '#app',
+  router,
   render: h => h(App)
 })
